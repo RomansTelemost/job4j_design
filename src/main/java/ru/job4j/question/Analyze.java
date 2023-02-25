83816cc9fa4ffd96a96aee48bf128b1c1290cc36
Roman Koshelev <termopsis@mail.ru>
2023-02-25 16:06:42 +0300

    3. Статистика по коллекции. [#45889] - refactor

--- src/main/java/ru/job4j/question/Analyze.java
+++ src/main/java/ru/job4j/question/Analyze.java
@@ -8,15 +8,6 @@ import java.util.Set;
 
 public class Analyze {
 
-
-    public static void main(String[] args) {
-        User u1 = new User(1, "A");
-        User u2 = new User(2, "B");
-        User u3 = new User(3, "C");
-        Set previous = Set.of(u1, u2, u3);
-        Set current = Set.of(u1, new User(2, "BB"), u3);
-        diff(previous, current);
-    }
     public static Info diff(Set<User> previous, Set<User> current) {
         int added = 0;
         int changed = 0;
