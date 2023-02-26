bb7f51badaadf84634fd9f9b9c9ac854385fba0f
Roman Koshelev <termopsis@mail.ru>
2023-02-26 23:38:04 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,68 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            bufferedReader.lines().forEach(line -> {
+                if (!line.isEmpty()
+                    && !line.startsWith("#")) {
+                    StringBuilder sb = new StringBuilder();
+                    if (!line.contains("=")) {
+                        sb.append("In line \'").append(line).append("\'").append(" not found symbol '='!");
+                        throw new IllegalArgumentException(sb.toString());
+                    }
+                    String[] keyAndValue = line.split("=");
+                    if (keyAndValue.length != 2) {
+                        sb.append("In line \'").append(line).append("\'").append(" not found key or value!");
+                        throw new IllegalArgumentException(sb.toString());
+                    }
+                    String[] key = keyAndValue[0].split("\\.");
+                    values.put(key[key.length - 1], keyAndValue[keyAndValue.length - 1]);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config("./data/pair_with_comment.properties");
+        config.load();
+
+        config.values.forEach((key, value) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Key: ").append(key).append(" - ").append("value: ").append(value);
+            System.out.println(sb);
+        });
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairWithComments() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("PostgreSQL")).isNull();
+    }
+
+    @Test
+    void whenPairWithoutEqualSymbolThenException() {
+        String path = "./data/pair_without_equalSymbol.properties";
+        Config config = new Config(path);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            config.load();
+        });
+        assertThat(exception.getMessage()).isEqualTo("In line 'hibernate.dialect' not found symbol '='!");
+    }
+
+    @Test
+    void whenPairWithoutKeyOrValueThenException() {
+        String path = "./data/pair_without_keyOrValue.properties";
+        Config config = new Config(path);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            config.load();
+        });
+        assertThat(exception.getMessage()).isEqualTo("In line 'hibernate.dialect=' not found key or value!");
+    }
+}
