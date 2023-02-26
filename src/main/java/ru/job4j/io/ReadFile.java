a6e118e03da6e085fead013e5995f4044ddc711e
Roman Koshelev <termopsis@mail.ru>
2023-02-26 13:01:30 +0300

    5.1. BufferedReader [#252489]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,9 +1,11 @@
 package ru.job4j.io;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class ReadFile {
     public static void main(String[] args) {
+        /**
         try (FileInputStream in = new FileInputStream("data/input.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
@@ -15,6 +17,13 @@ public class ReadFile {
                 System.out.println(line);
             }
             System.out.println(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+         */
+
+        try (BufferedReader in = new BufferedReader(new FileReader("data/input.txt"))) {
+            in.lines().forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
