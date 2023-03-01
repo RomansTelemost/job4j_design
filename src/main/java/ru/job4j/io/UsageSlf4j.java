0f9a1f80e1d3e8a97e25fad06bd7eeadba44477e
Roman Koshelev <termopsis@mail.ru>
2023-03-01 23:43:39 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageSlf4j.java
+++ src/main/java/ru/job4j/io/UsageSlf4j.java
@@ -13,5 +13,15 @@ public class UsageSlf4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        String name = "Petr Arsentev";
+        byte b = 1;
+        short s = 2;
+        int age = 33;
+        long l = 4;
+        double d = 5.4d;
+        float f = 6.6f;
+        boolean bool = true;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.debug("Byte : {}, short : {}, int : {}, long : {}, double : {}, float : {}, bool : {}", b, s, age, l, d, f, bool);
     }
 }
