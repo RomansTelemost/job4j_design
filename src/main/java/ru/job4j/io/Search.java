ece68b85b6aea176a8ecdb68a3a9dd35e125707a
Roman Koshelev <termopsis@mail.ru>
2023-03-12 22:06:28 +0300

    12. Валидация параметров запуска [#246865] - task

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
 import java.io.IOException;
+import java.io.InvalidObjectException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -10,13 +12,30 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateIncomingParameters(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validateIncomingParameters(String[] args) throws InvalidObjectException {
+        if (args.length != 2) {
+            throw new InvalidObjectException("Not found 2 parameters. Root folder and extension.");
+        }
+        try {
+            Paths.get(args[0]);
+        } catch (InvalidPathException e) {
+            throw new InvalidObjectException("Path to root folder is invalid.");
+        }
+        if (args[1].isBlank()
+                || !args[1].startsWith(".")
+                || args[1].length() < 2) {
+            throw new IllegalArgumentException("Extension data must starts with '.' and contains only letters");
+        }
     }
 }
