9162634d0dfc344ea178a467ddc940215fd734dd
Roman Koshelev <termopsis@mail.ru>
2023-03-12 20:31:31 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/DuplicatesFinder.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DuplicatesFinder {
+
+    public static void main(String[] args) throws IOException {
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), duplicatesVisitor);
+        duplicatesVisitor.doublePaths.entrySet().stream().filter((k) -> k.getValue().size() > 1).forEach(e -> {
+            System.out.println(e.getKey());
+            e.getValue().forEach(System.out::println);
+        });
+    }
+}
