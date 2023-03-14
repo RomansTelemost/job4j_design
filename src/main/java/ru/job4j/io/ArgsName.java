56547e5d184ef033c510ed990378516aac7be85d
Roman Koshelev <termopsis@mail.ru>
2023-03-14 20:54:39 +0300

    13. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("This key: '" + key + "' is missing");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        Arrays.stream(args).forEach(string -> {
+            String[] pair = string.split("=", 2);
+            if (pair[0].length() == 1) {
+                throw new IllegalArgumentException("Error: This argument '" + string + "' does not contain a key");
+            }
+            if (pair[1].length() == 0) {
+                throw new IllegalArgumentException("Error: This argument '" + string + "' does not contain a value");
+            }
+            values.put(pair[0].substring(1), pair[1]);
+        });
+    }
+
+    public static ArgsName of(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        Optional<String> optionalStringWithDash = Arrays.stream(args).filter(
+                string -> !string.startsWith("-")).findFirst();
+        if (optionalStringWithDash.isPresent()) {
+            throw new IllegalArgumentException("Error: This argument '" + optionalStringWithDash.get() + "' does not start with a '-' character");
+        }
+        Optional<String> optionalStringWithEquals = Arrays.stream(args).filter(
+                string -> !string.contains("=")).findFirst();
+        if (optionalStringWithEquals.isPresent()) {
+            throw new IllegalArgumentException("Error: This argument '" + optionalStringWithEquals.get() + "' does not contain an equal sign");
+        }
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
