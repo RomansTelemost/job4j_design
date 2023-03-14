30fd21640ec759b70be1ce137092054ffbc5f137
Roman Koshelev <termopsis@mail.ru>
2023-03-14 22:13:58 +0300

    13. Именованные аргументы [#295518] - fix

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -18,17 +18,11 @@ public class ArgsName {
     private void parse(String[] args) {
         Arrays.stream(args).forEach(string -> {
             String[] pair = string.split("=", 2);
-            if (pair[0].length() == 1) {
-                throw new IllegalArgumentException("Error: This argument '" + string + "' does not contain a key");
-            }
-            if (pair[1].length() == 0) {
-                throw new IllegalArgumentException("Error: This argument '" + string + "' does not contain a value");
-            }
             values.put(pair[0].substring(1), pair[1]);
         });
     }
 
-    public static ArgsName of(String[] args) {
+    private static void checkArgs(String[] args) {
         if (args.length == 0) {
             throw new IllegalArgumentException("Arguments not passed to program");
         }
@@ -42,6 +36,19 @@ public class ArgsName {
         if (optionalStringWithEquals.isPresent()) {
             throw new IllegalArgumentException("Error: This argument '" + optionalStringWithEquals.get() + "' does not contain an equal sign");
         }
+        Arrays.stream(args).forEach(string -> {
+            String[] pair = string.split("=", 2);
+            if (pair[0].length() == 1) {
+                throw new IllegalArgumentException("Error: This argument '" + string + "' does not contain a key");
+            }
+            if (pair[1].length() == 0) {
+                throw new IllegalArgumentException("Error: This argument '" + string + "' does not contain a value");
+            }
+        });
+    }
+
+    public static ArgsName of(String[] args) {
+        checkArgs(args);
         ArgsName names = new ArgsName();
         names.parse(args);
         return names;
