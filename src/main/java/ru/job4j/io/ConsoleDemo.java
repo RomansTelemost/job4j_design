867f203183934c9c6bef8703e8c0492237f8359f
Roman Koshelev <termopsis@mail.ru>
2023-03-19 20:54:55 +0300

    18. Класс Console [#504991]

---
+++ src/main/java/ru/job4j/io/ConsoleDemo.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.Console;
+import java.util.Arrays;
+
+public class ConsoleDemo {
+
+    /**
+     * java -Dfile.encoding=utf8 ConsoleDemo.java
+     */
+    public static void main(String[] args) {
+        String login;
+        char[] charPassword;
+        Console console = System.console();
+        if (console == null) {
+            System.out.println("Консоль недоступна");
+            return;
+        }
+        login = console.readLine("%s", "Введите логин: ");
+        console.printf("Ваш логин: %s\n", login);
+        charPassword = console.readPassword("%s", "Введите пароль: ");
+        System.out.println("Ваш пароль: " + String.valueOf(charPassword));
+        Arrays.fill(charPassword, ' ');
+    }
+}
