6530a14d1a38cb01849500f046f27db07a938b44
Roman Koshelev <termopsis@mail.ru>
2023-04-02 15:50:13 +0300

    1. Inner join [#373617] - task

---
+++ db/dql/2.3.2. Query/1. Inner join [#373617]/script.sql
@@ -0,0 +1,20 @@
+create table if not exists customer(
+	id serial primary key,
+	name varchar(255)
+);
+
+create table if not exists contact(
+	id serial primary key,
+	phone text,
+	customer_id int references customer(id)
+);
+
+insert into customer(name) values('Ilon');
+insert into customer(name) values('Simen');
+insert into customer(name) values('Dmitryi');
+
+insert into contact(phone, customer_id) values('+1-913-222-99-43', 1);
+insert into contact(phone, customer_id) values('+7-913-222-99-44', 2);
+
+select cus.name, con.phone from customer as cus
+join contact as con on cus.id = con.customer_id;
