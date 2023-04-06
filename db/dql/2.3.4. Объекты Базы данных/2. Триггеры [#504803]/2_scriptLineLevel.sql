ca6af937f06466a28b922b5d4c6ca2944f8867ce
Roman Koshelev <termopsis@mail.ru>
2023-04-06 23:28:42 +0300

    2. Триггеры [#504803]

---
+++ db/dql/2.3.4. Объекты Базы данных/2. Триггеры [#504803]/2_scriptLineLevel.sql
@@ -0,0 +1,17 @@
+create or replace function tax()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price - price * 0.2
+        where id = (select id from inserted) and count <= 5;
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_trigger
+    after insert on products
+    referencing new table as inserted
+    for each statement
+    execute procedure tax();
