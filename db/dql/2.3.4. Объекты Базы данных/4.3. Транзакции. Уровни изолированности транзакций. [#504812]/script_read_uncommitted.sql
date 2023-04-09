8e45c5755300af0667ddf63c346bdfffd710e405
Roman Koshelev <termopsis@mail.ru>
2023-04-09 20:29:25 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

---
+++ db/dql/2.3.4. Объекты Базы данных/4.3. Транзакции. Уровни изолированности транзакций. [#504812]/script_read_uncommitted.sql
@@ -0,0 +1,12 @@
+create table if not exists products (
+    id serial primary key,
+    name varchar(50),
+    count integer default 0,
+    price integer
+);
+
+insert into products (name, count, price) VALUES ('product_1', 3, 50);
+insert into products (name, count, price) VALUES ('product_2', 15, 32);
+insert into products (name, count, price) VALUES ('product_3', 8, 115);
+
+set session transaction isolation level read uncommitted;
