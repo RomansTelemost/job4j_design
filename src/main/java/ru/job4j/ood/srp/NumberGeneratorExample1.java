9b8d1af878148adc3400ea79735a1964333ea2bd
Roman Koshelev <termopsis@mail.ru>
2023-05-07 16:53:16 +0300

    SRP - fix 2

--- src/main/java/ru/job4j/ood/srp/NumberGeneratorExample1.java
+++ src/main/java/ru/job4j/ood/srp/NumberGeneratorExample1.java
@@ -1,10 +1,11 @@
 package ru.job4j.ood.srp;
 
 import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class NumberGeneratorExample1 implements NumberGenerator {
 
-    private final Random random;
+    private final RandomGenerator random;
 
     public NumberGeneratorExample1(Random random) {
         this.random = random;
